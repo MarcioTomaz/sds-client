da2d01fb269df4983b194d075b8b5c19248b12e5
MarcioTomaz <marciotmaz@gmail.com>
2022-01-22 19:17:11 -0300

    add: first commit

---
+++ src/test/java/com/dsClient/demo/DsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsClient.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
